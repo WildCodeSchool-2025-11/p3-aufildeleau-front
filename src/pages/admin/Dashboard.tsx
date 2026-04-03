b3a920a2edbe6943a16c3b848652a0fd5296424d
yavuzkutuk <yavuz@kutuk.fr>
2026-04-03 10:35:56 +0200

    ADD: Multer upload file

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState } from "react";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+  const [files, setFiles] = useState("");
+
+  const handleSubmit = async () => {
+    const token = localStorage.getItem("token");
+
+	const formData = new FormData();
+	formData.append('email', 'yavuz@kutuk.fr');
+	if(files){
+		formData.append('avatar', files);
+	}
+
+    fetch("http://localhost:3310/api/users/avatar/6", {
+      method: "PUT",
+      headers: { Authorization: `x-token ${token}` },
+      body: formData,
+    });
+  };
+
+  console.log(files);
+
+  return (
+    <div>
+      Dashboard
+      <p>
+          <input
+            type="file"
+            name="avatar"
+            onChange={(e) => setFiles(e.target.files[0] || null)}
+          />
+          <button onClick={handleSubmit}>Mettre a jour</button>
+      </p>
+    </div>
+  );
 };
 
 export default Dashboard;
